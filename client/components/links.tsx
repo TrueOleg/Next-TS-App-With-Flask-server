a6e4836ca2fde6f4275a3f71fb16dd2fd54ed43b
trueoleg <trueolegzaichkin@gmail.com>
2019-11-01 13:12:48 +0300

    first commit

--- client/components/links.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Links() {
-  return <ul>
-    <li><a href={"/"}>Home</a></li>
-    <li><a href={"/login"}>Login</a></li>
-    <li><a href={"/dashboard"}>Dashboard (protected)</a></li>
-  </ul>
-}
--- client/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Links } from "../components/links"
-
-function Index() {
-  return <Links />;
-}
-
-export default Index;
